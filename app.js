3953190c6263b60a58e59377f7735bd24b36f118
Om Priyansh <ompriyansh24@gmail.com>
2024-01-21 04:04:29 +0530

    third commit

--- app.js
+++ app.js
@@ -5,32 +5,69 @@ const followersContainer = document.querySelector(".profile-followers");
 const reposContainer = document.querySelector(".profile-repos");
 const followingContainer = document.querySelector(".profile-following");
 const fetchContainer = document.querySelector(".profile-container");
+const profilepicContainer = document.querySelector(".profile_pic");
+var repoButton = document.querySelector(".profile_button")
 
 const fetchUsers = async (user) => {
-    const api_call = await fetch(`https://api.github.com/users/${user}`)
-    const data = await api_call.json();
-    return {data: data};
+    try{
+        const api_call = await fetch(`https://api.github.com/users/${user}`);
+        const data = await api_call.json();
+        return {data: data};
+    }catch(err){
+        alert("Please enter Valid Github Username");
+    }
 
 }
 
-const showdata = () =>{
-    fetchUsers(inputValue.value).then((res) =>{
-        console.log(res);
+const openUrl = () =>{
+    try{
+        fetchUsers(inputValue.value).then((res) =>{
+            console.log(res);
+        window.open(`${res.data.html_url}`);}).catch(error =>{
+            alert("error");})
+    }catch(err){
+        alert("Please enter Valid Github Username");
+    }
+}
 
-        nameContainer.innerHTML = `Name: ${res.data.name}`;
-        followersContainer.innerHTML = `Followers:   ${res.data.followers}`;
-        followingContainer.innerHTML = `Following:   ${res.data.following}`;
-        reposContainer.innerHTML = `Repos: ${res.data.public_repos}`;
+const showdata = () =>{
+    try{
+        fetchUsers(inputValue.value).then((res) =>{
+            // console.log(res);
         
+        
+            nameContainer.innerHTML = `Name:<br> ${res.data.name}`;
+            followersContainer.innerHTML = `Followers:<br>   ${res.data.followers}`;
+            followingContainer.innerHTML = `Following:<br>   ${res.data.following}`;
+            reposContainer.innerHTML = `Repos:<br> ${res.data.public_repos}`;
+            profilepicContainer.innerHTML= `<img src = "${res.data.avatar_url}" alt = "Profile Picture" width = 100px>`
+            repoButton.classList.add("Button");
+            repoButton.innerHTML = 'Open Profile';
+        
+    }).catch(error =>{
+        alert("error");
     })
     fetchContainer.classList.add("animate");
     setTimeout(function() {
         fetchContainer.classList.remove("animate");
     }, 2000);
+    }catch(err){
+        alert("Please enter Valid Github Username");
+    }
     
 }
 
 searchButton.addEventListener("click", ()=>{
-    showdata();
+    console.log(inputValue.value);
+    if (inputValue.value == ""){
+        alert("Please enter Valid Github Username");
+    }
+    else{
+        showdata();
+    }
+    
 })
 
+repoButton.addEventListener("click", ()=>{
+    openUrl();
+})
